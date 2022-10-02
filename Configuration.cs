0e69aefa18be2fb20a121b677dd8bac8eb6a0b0a
gamous <gammarayburst@163.com>
2022-10-02 17:22:38 +0800

    代码整理、泛用性加强 以及一个配置窗口

--- Configuration.cs
+++ Configuration.cs
@@ -2,13 +2,14 @@
 
 namespace Lifu
 {
-    public class Configuration : IPluginConfiguration
-    {
-        public int Version { get; set; }
-        public bool undiyici;
+    public class Configuration : IPluginConfiguration{
+        public int Version { get; set; } = 0;
         public void Initialize() { }
-
-
         public void Save() => DalamudApi.PluginInterface.SavePluginConfig(this);
+
+        public int LeveQuestId { get; set; } = 1635;
+        public int LeveItemMagic { get; set; } = 2005;
+        public string LeveNpc1 { get; set; } = "格里格";
+        public string LeveNpc2 { get; set; } = "阿尔德伊恩";
     }
 }
